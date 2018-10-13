c18cd4e93ca00b023cbad5ccc8563fbb588a311a
blueseanz <ming.hui.fengnz@gmail.com>
2018-10-14 08:48:04 +1300

    add business object layer

---
+++ EmployeeInformationSystem/BusinessObjectLayer/Role.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BusinessObjectLayer
+{
+    [Table("Role")]
+    public partial class Role
+    {
+        [Key]
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+        public string RoleCode { get; set; }
+
+        public virtual IEnumerable<Employee> Employees { get; set; }
+    }
+}
