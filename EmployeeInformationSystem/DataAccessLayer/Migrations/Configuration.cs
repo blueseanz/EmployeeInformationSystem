a4bb25bb6e43029c5a7935391734b95e39dca0d7
blueseanz <ming.hui.fengnz@gmail.com>
2018-10-15 22:09:37 +1300

    Enable Code-First Migrations

---
+++ EmployeeInformationSystem/DataAccessLayer/Migrations/Configuration.cs
@@ -0,0 +1,25 @@
+namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DataAccessLayer.EISDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+            ContextKey = "DataAccessLayer.EISDBContext";
+        }
+
+        protected override void Seed(DataAccessLayer.EISDBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
